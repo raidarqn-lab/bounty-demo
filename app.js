17f470a25dffac6f13b488cc757315baa4601c0f
raidarqn-lab <raidarqn@gmail.com>
2026-09-23 21:20:09 -0700

    Rename event filter to Desert Storm/Canyon Storm

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import {locale, initLanguages} from './i18n.js?v=5';
+import {locale, initLanguages} from './i18n.js?v=6';
 import {generateWeek,shift,themes} from './schedule.js?v=5';
 const $=s=>document.querySelector(s),e=s=>String(s).replace(/[&<>"']/g,c=>({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'}[c]));
 const demoNow=Date.parse('2026-09-24T04:00:00Z');let week='2026-09-21',filter='all',statusFilter='all',tab='available',selected=null,day=null;const opponents=new Map([['2026-09-21',{server:'1603',tag:'UNIi'}]]),receipts=new Map();
